a525f61d4ef9154e85e6819382fb2dc076eb5005
Jay Khatri <jaykhatrimail@gmail.com>
2022-07-23 13:44:39 -0700

    add nprogress animation for page load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,15 @@
+import Router from 'next/router';
+import nProgress from 'nprogress';
+
 import '../styles/globals.scss';
+import '../styles/nprogress.css';
 import '../styles/public.css';
+
 import type { AppProps } from 'next/app';
+
+Router.events.on('routeChangeStart', nProgress.start);
+Router.events.on('routeChangeError', nProgress.done);
+Router.events.on('routeChangeComplete', nProgress.done);
 
 function MyApp({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />;
