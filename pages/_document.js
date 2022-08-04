6b074b92d97c66e4f17813c110e885d8ed181cb8
Jay Khatri <jaykhatrimail@gmail.com>
2022-08-04 10:24:50 -0700

    changes

--- pages/_document.js
+++ pages/_document.js
@@ -7,6 +7,7 @@ class HighlightDocument extends Document {
     return (
       <Html>
         <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link
             rel="preconnect"
             href="https://fonts.gstatic.com"
