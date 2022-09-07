9620744407a149e39e136bd1917d4d25e8ecee5c
Vadim Korolik <vkorolik@gmail.com>
2022-09-07 11:44:52 -0700

    rename variable

--- middleware.ts
+++ middleware.ts
@@ -1,14 +1,14 @@
 import { NextRequest, NextResponse } from 'next/server';
 
 // this will redirect the domain landing page to the following page component
-const SUBDOMAIN_REDIRECTS = {
+const SUBDOMAIN_LANDING_PAGES = {
   observability: '/blog',
 };
 
 export default function middleware(req: NextRequest) {
   const { pathname } = req.nextUrl;
   const hostname = req.headers.get('host');
-  for (const [k, v] of Object.entries(SUBDOMAIN_REDIRECTS)) {
+  for (const [k, v] of Object.entries(SUBDOMAIN_LANDING_PAGES)) {
     if (hostname?.startsWith(`${k}.`)) {
       if (pathname === '/') {
         const domain = process.env.NEXT_PUBLIC_VERCEL_URL || req.nextUrl.origin;
