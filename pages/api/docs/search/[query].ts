96016f3c63cc172a3829286b18126d1ad7d5a5b3
yihongat <yihonganthony@gmail.com>
2022-10-14 23:11:37 -0400

    added types

--- pages/api/docs/search/[query].ts
+++ pages/api/docs/search/[query].ts
@@ -1,6 +1,7 @@
 import { promises as fsp } from 'fs';
+import type { NextApiRequest, NextApiResponse } from 'next';
 
-export default function handler(req, res) {
+export default function handler(req: NextApiRequest, res: NextApiResponse) {
   console.log(req.query);
   return res.json({});
 }
