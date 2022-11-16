9e7290eb1c37e23b851f6045f18907b018cca668
Abhishek <abhimore74@gmail.com>
2022-11-16 14:59:55 -0600

    modified dropdown

--- components/common/Navbar/Navbar.tsx
+++ components/common/Navbar/Navbar.tsx
@@ -94,7 +94,7 @@ const ResourceDropdown = ({
       <Transition
         show={isShowing}
         onMouseEnter={() => setIsShowing(true)}
-        onMouseLeave={() => setIsShowing(true)}
+        onMouseLeave={() => setIsShowing(false)}
         enter="transition ease-out duration-200"
         enterFrom="opacity-0 translate-y-1"
         enterTo="opacity-100 translate-y-0"
