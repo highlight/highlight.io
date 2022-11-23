5c3568da9b023e8c0f077f97f64e0dfece873232
Aaron <19144605+Crawron@users.noreply.github.com>
2022-11-22 22:47:05 -0500

    Blog posts page listing (#118)
    draft page currently in `/blog/posts`
    
    mostly organized but still gotta move components out of the one file
    it's in, so they're more reusable in other pages
    
    I also gotta update the design, I've noticed it's changed slightly
    
    Co-authored-by: Vadim Korolik <vkorolik@gmail.com>

--- components/Blog/SuggestedBlogPost/SuggestedBlogPost.tsx
+++ components/Blog/SuggestedBlogPost/SuggestedBlogPost.tsx
@@ -4,6 +4,7 @@ import Link from 'next/link';
 import { Typography } from '../../common/Typography/Typography';
 import { Post } from '../BlogPost/BlogPost';
 import classNames from 'classnames';
+import { PostTag } from '../Tag';
 
 export const SuggestedBlogPost = ({
   slug,
@@ -11,11 +12,15 @@ export const SuggestedBlogPost = ({
   image,
   title,
   publishedAt,
-  tags,
+  tags_relations,
   readingTime,
 }: Post) => {
   return (
-    <Link href={`/blog/${slug}`} style={{ textDecoration: 'none' }}>
+    <Link
+      href={`/blog/${slug}`}
+      style={{ textDecoration: 'none' }}
+      legacyBehavior
+    >
       <div className={classNames(styles.blogPost, styles.suggestedBlogPost)}>
         <div className={styles.cardSection}>
           <div className={styles.cardImage}>
@@ -46,17 +51,8 @@ export const SuggestedBlogPost = ({
             </Typography>
           </div>
           <div className={styles.tagDiv}>
-            {tags.map((tag: string) => (
-              <Link
-                key={tag}
-                href={`/blog?tag=${tag}`}
-                passHref={true}
-                legacyBehavior
-              >
-                <div>
-                  <Typography type="copy3">{tag}</Typography>
-                </div>
-              </Link>
+            {tags_relations.map((tag) => (
+              <PostTag {...tag} key={tag.slug + slug} />
             ))}
           </div>
         </div>
