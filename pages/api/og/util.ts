7666c647f802d53a97cfe5e0f08aeb1d1055007f
Jay Khatri <jaykhatrimail@gmail.com>
2023-01-04 17:27:41 +0900

    changes

--- pages/api/og/util.ts
+++ pages/api/og/util.ts
@@ -12,3 +12,18 @@ export const hero = fetch(
   // @ts-ignore
   new URL('../../../public/images/hero.png', import.meta.url)
 ).then((res) => res.arrayBuffer());
+
+export const logoOnDark = fetch(
+  // @ts-ignore
+  new URL('../../../public/images/logo-on-dark.png', import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export const bug1 = fetch(
+  // @ts-ignore
+  new URL('../../../public/images/bug1.png', import.meta.url)
+).then((res) => res.arrayBuffer());
+
+export const bug2 = fetch(
+  // @ts-ignore
+  new URL('../../../public/images/bug2.png', import.meta.url)
+).then((res) => res.arrayBuffer());
