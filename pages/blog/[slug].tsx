22f0c2a81ba5be9396a9c33ad53af07e1f23dc25
Crawron <azertyaaron@gmail.com>
2023-01-04 14:46:28 -0500

    add back to blog link in blogpost

--- pages/blog/[slug].tsx
+++ pages/blog/[slug].tsx
@@ -20,7 +20,7 @@ import { SuggestedBlogPost } from '../../components/Blog/SuggestedBlogPost/Sugge
 import { ElementNode } from '@graphcms/rich-text-types';
 import { Post } from '../../components/Blog/BlogPost/BlogPost';
 import { Meta } from '../../components/common/Head/Meta';
-import { FaGithub, FaGlobe, FaLinkedin, FaTwitter } from 'react-icons/fa';
+import ReturnIcon from '../../public/images/ReturnIcon';
 import { HighlightCodeBlock } from '../../components/Docs/HighlightCodeBlock/HighlightCodeBlock';
 import { GraphQLRequest } from '../../utils/graphql';
 import { PostTag } from '../../components/Blog/Tag';
@@ -332,7 +332,8 @@ const PostPage = ({
         canonical={`/blog/${post.slug}`}
       />
       <BlogNavbar title={post.title} endPosition={endPosition} />
-      <main ref={blogBody} className={styles.mainBlogPadding}>
+      <main ref={blogBody} className={classNames(styles.mainBlogPadding, "relative")}>
+        <Link href="/blog" className='absolute flex flex-row gap-2 place-items-center top-6 left-8'><ReturnIcon /> Back to blog</Link>
         <Section>
           <div className={homeStyles.anchorTitle}>
             <Typography type="copy2">
