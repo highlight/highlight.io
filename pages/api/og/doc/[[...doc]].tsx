bfb389bfb18e53717aaf7d36c221acc90190e6f0
Jay Khatri <jaykhatrimail@gmail.com>
2023-01-04 20:17:49 +0900

    lets go

--- pages/api/og/doc/[[...doc]].tsx
+++ pages/api/og/doc/[[...doc]].tsx
@@ -59,7 +59,7 @@ export default async function handler(req: NextRequest) {
         />
         <div style={{ display: "flex", flexDirection: "column", alignItems: 'center' }}>
           <div style={{ marginBottom: 20, fontSize: 45, fontFamily: 'PoppinsLight', color: '#dfdfdf', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
-            Highlight / {docPath?.includes("sdk") ? "SDK Docs" : "General Docs"}
+            {docPath?.split("/").reverse().slice(1).map(s => s.split("-").map(string => string.charAt(0).toUpperCase() + string.slice(1)).join(" ")).join(" / ")}
           </div>
           <div style={{ fontSize: 75, fontFamily: 'Poppins', color: 'white', width: '100%', display: 'flex', alignItems: 'center', justifyContent: 'center' }}>
             {JSON.stringify(d?.meta)}
