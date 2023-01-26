eff36d2dd373240b852db390eef5a14980ba935e
Jay Khatri <jaykhatrimail@gmail.com>
2023-01-25 20:44:25 -0800

    add margins

--- components/GithubPopup/GithubPopup.tsx
+++ components/GithubPopup/GithubPopup.tsx
@@ -20,94 +20,100 @@ export const GithubPopup = () => {
 const MobileGithubPopup = () => {
     const [hide, setHide] = useState(false);
     return (
-        <motion.div
-            initial={{ bottom: -200 }}
-            animate={{
-                bottom: -38,
-            }}
-            transition={{
-                type: "spring",
-                bounce: 0.4,
-                duration: 0.8,
-                delay: 1
-            }}
-            style={{
-                position: "fixed",
-                left: "50%",
-                transform: "translate(-50%, -50%)",
-                zIndex: 2000,
-            }}
-        >
-            <div
-                className={styles.mobilePopup}
+        hide ? <></> :
+            <motion.div
+                initial={{ bottom: -200 }}
+                animate={{
+                    bottom: -38,
+                }}
+                transition={{
+                    type: "spring",
+                    bounce: 0.4,
+                    duration: 0.8,
+                    delay: 1
+                }}
+                style={{
+                    position: "fixed",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    zIndex: 2000,
+                }}
             >
-                <div style={{
-                    fontSize: 18,
-                    fontWeight: "400",
-                    color: "white",
-                    overflow: "hidden",
-                    whiteSpace: "nowrap",
-                }}>
-                    Star on GitHub?
-                </div>
-                <div style={{ marginTop: 0 }}>
-                    <GitHubButton
-                        href="https://github.com/highlight/highlight"
-                        data-color-scheme="no-preference: light; light: light; dark: light;"
-                        data-size="large"
-                        data-show-count="true"
-                        aria-label="Star highlight/highlight on GitHub">Star
-                    </GitHubButton>
-                </div>
-                <button onClick={() => setHide(true)}>
-                    <CrossIcon />
-                </button>
-            </div >
-        </motion.div>);
+                <div
+                    className={styles.mobilePopup}
+                >
+                    <div style={{
+                        fontSize: 18,
+                        fontWeight: "400",
+                        color: "white",
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                    }}>
+                        Star on GitHub?
+                    </div>
+                    <div style={{ marginTop: 4 }}>
+                        <GitHubButton
+                            href="https://github.com/highlight/highlight"
+                            data-color-scheme="no-preference: light; light: light; dark: light;"
+                            data-size="large"
+                            data-show-count="true"
+                            aria-label="Star highlight/highlight on GitHub">Star
+                        </GitHubButton>
+                    </div>
+                    <button onClick={() => setHide(true)}>
+                        <CrossIcon />
+                    </button>
+                </div >
+            </motion.div>
+    );
 }
 
 const DesktopGithubPopup = () => {
     const [hide, setHide] = useState(false);
-    return (<motion.div
-        initial={{ bottom: -200 }}
-        animate={{
-            bottom: 10,
-        }}
-        transition={{
-            type: "spring",
-            bounce: 0.4,
-            duration: 0.8,
-            delay: 1
-        }}
-        style={{
-            position: "fixed",
-            left: "50%",
-            transform: "translate(-50%, -50%)",
-            zIndex: 2000,
-        }}
-    >
-        <div
-            className={styles.desktopPopup}
-        >
-            <div style={{
-                fontSize: 18,
-                fontWeight: "400",
-                color: "white",
-                overflow: "hidden",
-                whiteSpace: "nowrap",
-            }}>
-                Star us on GitHub
-            </div>
-            <GitHubButton
-                href="https://github.com/highlight/highlight"
-                data-color-scheme="no-preference: light; light: light; dark: light;"
-                data-size="large"
-                data-show-count="true"
-                aria-label="Star highlight/highlight on GitHub">Star
-            </GitHubButton>
-            <button onClick={() => setHide(true)}>
-                <CrossIcon />
-            </button>
-        </div >
-    </motion.div >);
+    return (
+        hide ? <></> :
+            <motion.div
+                initial={{ bottom: -200 }}
+                animate={{
+                    bottom: 10,
+                }}
+                transition={{
+                    type: "spring",
+                    bounce: 0.4,
+                    duration: 0.8,
+                    delay: 1
+                }}
+                style={{
+                    position: "fixed",
+                    left: "50%",
+                    transform: "translate(-50%, -50%)",
+                    zIndex: 2000,
+                }}
+            >
+                <div
+                    className={styles.desktopPopup}
+                >
+                    <div style={{
+                        fontSize: 18,
+                        fontWeight: "400",
+                        color: "white",
+                        overflow: "hidden",
+                        whiteSpace: "nowrap",
+                    }}>
+                        Star us on GitHub
+                    </div>
+                    <div style={{ marginTop: 4 }}>
+                        <GitHubButton
+                            href="https://github.com/highlight/highlight"
+                            data-color-scheme="no-preference: light; light: light; dark: light;"
+                            data-size="large"
+                            data-show-count="true"
+                            aria-label="Star highlight/highlight on GitHub">Star
+                        </GitHubButton>
+                    </div>
+                    <button onClick={() => setHide(true)}>
+                        <CrossIcon />
+                    </button>
+                </div >
+            </motion.div >);
 }
