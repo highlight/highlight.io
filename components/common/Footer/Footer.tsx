1f306a96b5eb85ddeb641a5ab5739d4bb1355850
Abhishek <abhimore74@gmail.com>
2023-02-02 22:27:01 -0600

    edited github links

--- components/common/Footer/Footer.tsx
+++ components/common/Footer/Footer.tsx
@@ -19,7 +19,7 @@ const Footer = () => {
             <a href="https://www.linkedin.com/company/highlightrun">
               <FaLinkedinIn className={styles.socialIcon} />
             </a>
-            <a href="https://github.com/highlight-run">
+            <a href="https://github.com/highlight">
               <FaGithub className={styles.socialIcon} />
             </a>
           </div>
