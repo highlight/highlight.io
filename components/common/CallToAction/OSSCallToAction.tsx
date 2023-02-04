1a535912edce589d057523aa8e8b2952be59363c
Abhishek <abhimore74@gmail.com>
2023-02-03 20:57:00 -0600

    modified line height and text centering

--- components/common/CallToAction/OSSCallToAction.tsx
+++ components/common/CallToAction/OSSCallToAction.tsx
@@ -9,13 +9,13 @@ import classNames from 'classnames';
 
 export const OSSCallToAction = () => {
   return (
-    <div className={"flex justify-center mx-5 md:mx-[10vw] mt-32 mb-40"}>
+    <div className={"flex justify-center mx-5 md:mx-[10vw] my-10 md:my-40"}>
       <div className={classNames(styles.ossCard, "w-full max-w-[1250px] border-[1px] border-divider-on-dark rounded-3xl py-10")}>
-        <h3 className="text-center">
-          Join our <span className={styles.highlightedText}>Open-Source</span> Community.
+        <h3 className="text-center leading-normal">
+          Join our <span className={styles.highlightedText}>open source</span> Community.
         </h3>
-        <div className="text-center px-16 mt-6">
-          <Typography type="copy1" >
+        <div className="text-center px-2 md:px-16 mt-6">
+          <Typography type="copy1" className="leading-relaxed">
             Have a feature request? Or want to build the future of Highlight?<br />
             Check us out and join the fun!
           </Typography>
@@ -24,7 +24,7 @@ export const OSSCallToAction = () => {
           <div className="flex flex-col lg:flex-row justify-center gap-3 lg:gap-4"
           >
             <PrimaryButton href="https://github.com/highlight/">
-              <div className="flex items-center gap-3">
+              <div className="flex justify-center items-center gap-3">
                 <AiFillGithub className="w-5 h-5 mb-[2px]" />
                 <Typography type="copy2" emphasis={true}>
                   Github Repo
