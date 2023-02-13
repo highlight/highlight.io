e390de186765122266717c301f6172b7e4eda712
Crawron <azertyaaron@gmail.com>
2023-02-13 14:56:05 -0500

    formatting

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,13 +3,13 @@ module.exports = {
   content: [
     './pages/**/*.{js,ts,jsx,tsx}',
     './components/**/*.{js,ts,jsx,tsx}',
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/index.tsx",
-    "./pages/*.{js,ts,jsx,tsx}",
-    "./components/Products/*.{js,ts,jsx,tsx}",
-    "./components/Products/FeatureBox.tsx",
-    "./components/common/Navbar/*.{js,ts,jsx,tsx}",
-    "./components/common/Navbar/Navbar.tsx",
+    './app/**/*.{js,ts,jsx,tsx}',
+    './pages/index.tsx',
+    './pages/*.{js,ts,jsx,tsx}',
+    './components/Products/*.{js,ts,jsx,tsx}',
+    './components/Products/FeatureBox.tsx',
+    './components/common/Navbar/*.{js,ts,jsx,tsx}',
+    './components/common/Navbar/Navbar.tsx',
   ],
   theme: {
     extend: {
