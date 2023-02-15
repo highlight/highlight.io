24cf11c692d45a197795756c8f47d9c43e7889df
Vadim Korolik <vkorolik@gmail.com>
2023-02-15 15:42:27 -0800

    update images and contents

--- components/Home/FeatureCarousel/DesktopCard.tsx
+++ components/Home/FeatureCarousel/DesktopCard.tsx
@@ -1,15 +1,25 @@
-import React from "react";
-import { Feature } from "./FeatureCarousel";
-import Image from "next/image";
-import { PrimaryButton } from "../../common/Buttons/PrimaryButton";
-import { Typography } from "../../common/Typography/Typography";
+import React from 'react'
+import { Feature } from './FeatureCarousel'
+import Image from 'next/image'
+import { PrimaryButton } from '../../common/Buttons/PrimaryButton'
+import { Typography } from '../../common/Typography/Typography'
+import { HighlightCodeBlock } from '../../Docs/HighlightCodeBlock/HighlightCodeBlock'
+import highlightCodeTheme from '../../common/CodeBlock/highlight-code-theme'
+import { Code } from 'react-code-blocks'
 
-export const DesktopCard = (props: { feature: Feature, index: number }) => {
+export const DesktopCard = (props: { feature: Feature; index: number }) => {
   return (
-    <div key={props.index} className="relative flex justify-end px-5 lg:px-16 w-full  lg:h-[450px] xl:h-[525px] md:border-[1px] border-divider-on-dark lg:rounded-br-lg lg:rounded-bl-lg flex-shrink-0 snap-always snap-center">
+    <div
+      key={props.index}
+      className="relative flex justify-end px-5 lg:px-16 w-full  lg:h-[450px] xl:h-[525px] md:border-[1px] border-divider-on-dark lg:rounded-br-lg lg:rounded-bl-lg flex-shrink-0 snap-always snap-center"
+    >
       <div className="hidden sm:flex w-1/2">
         <div className="absolute bottom-0 left-0">
-          <Image className="object-contain sm:w-[350px] md:w-[450px] lg:w-[500px] xl:w-[550px]" src={props.feature.desktopImage} alt="Feature Spotlight" />
+          <Image
+            className="object-contain sm:w-[350px] md:w-[450px] lg:w-[500px] xl:w-[550px] sm:h-[325px] md:h-[375px] lg:h-[425px] xl:h-[520px]"
+            src={props.feature.desktopImage}
+            alt="Feature Spotlight"
+          />
         </div>
       </div>
       <div className="flex flex-col gap-4 justify-start md:pt-8 xl:justify-center text-left w-full sm:w-2/5">
@@ -18,29 +28,66 @@ export const DesktopCard = (props: { feature: Feature, index: number }) => {
           <h4 className="sm:hidden">{props.feature.title}</h4>
           <Typography type="copy3">{props.feature.description}</Typography>
         </div>
-        <div className="flex flex-col gap-5 py-4 my-3 bg-color-divider-on-dark rounded-lg px-4">
-          <div className="flex items-center gap-2">
-            {props.feature.featureImage1}
-            <Typography type="copy3" className="text-color-copy-on-dark" emphasis>{props.feature.feature1}</Typography>
+        {props.feature.code ? (
+          <Code
+            language={'bash'}
+            text={props.feature.code.join('\n')}
+            showLineNumbers={false}
+            theme={highlightCodeTheme}
+            wrapLines
+          />
+        ) : (
+          <div className="flex flex-col gap-5 py-4 my-3 bg-color-divider-on-dark rounded-lg px-4">
+            <div className="flex items-center gap-2">
+              {props.feature.featureImage1}
+              <Typography
+                type="copy3"
+                className="text-color-copy-on-dark"
+                emphasis
+              >
+                {props.feature.feature1}
+              </Typography>
+            </div>
+            <div className="flex items-center gap-2">
+              {props.feature.featureImage2}
+              <Typography
+                type="copy3"
+                className="text-color-copy-on-dark"
+                emphasis
+              >
+                {props.feature.feature2}
+              </Typography>
+            </div>
+            <div className="flex items-center gap-2">
+              {props.feature.featureImage3}
+              <Typography
+                type="copy3"
+                className="text-color-copy-on-dark"
+                emphasis
+              >
+                {props.feature.feature3}
+              </Typography>
+            </div>
           </div>
-          <div className="flex items-center gap-2">
-            {props.feature.featureImage1}
-            <Typography type="copy3" className="text-color-copy-on-dark" emphasis>{props.feature.feature2}</Typography>
-          </div>
-          <div className="flex items-center gap-2">
-            {props.feature.featureImage1}
-            <Typography type="copy3" className="text-color-copy-on-dark" emphasis>{props.feature.feature3}</Typography>
-          </div>
-        </div>
+        )}
         <div className="flex justify-start mb-4">
-          <PrimaryButton href={props.feature.link} className='py-[6px] text-center w-full sm:w-auto'>
-            <Typography type="copy3" emphasis={true}>Learn More</Typography>
+          <PrimaryButton
+            href={props.feature.link}
+            className="py-[6px] text-center w-full sm:w-auto"
+          >
+            <Typography type="copy3" emphasis={true}>
+              Learn More
+            </Typography>
           </PrimaryButton>
         </div>
         <div className="flex sm:hidden justify-center w-full ">
-          <Image className="object-cover" src={props.feature.mobileImage} alt="Feature Spotlight" />
+          <Image
+            className="object-cover"
+            src={props.feature.mobileImage}
+            alt="Feature Spotlight"
+          />
         </div>
       </div>
     </div>
   )
-}; 
+}
--- components/Home/FeatureCarousel/FeatureCarousel.tsx
+++ components/Home/FeatureCarousel/FeatureCarousel.tsx
@@ -1,17 +1,35 @@
-import styles from '../Home.module.scss';
-import classNames from 'classnames';
-import { Typography } from '../../../components/common/Typography/Typography';
-import { useEffect, useState } from 'react';
-import { StaticImageData } from 'next/image';
-import featureImg1 from '../../../public/images/featureCarousel1.png';
-import landingCarousel1 from '../../../public/images/landingCarousel1.png';
-import tempCarouselImage from '../../../public/images/tempCarouselImage.png';
+import styles from '../Home.module.scss'
+import classNames from 'classnames'
+import { Typography } from '../../common/Typography/Typography'
+import { useEffect, useState } from 'react'
+import { StaticImageData } from 'next/image'
+import sessionReplay from '../../../public/images/session-replay.png'
+import errorMonitoring from '../../../public/images/error-monitoring.png'
+import fullstackLogging from '../../../public/images/fullstack-logging.png'
+import selfHosting from '../../../public/images/self-hosting.png'
+import openSource from '../../../public/images/open-source.png'
+import tempCarouselImage from '../../../public/images/tempCarouselImage.png'
 import useEmblaCarousel from 'embla-carousel-react'
-import { HiTerminal, HiFilm, HiLightningBolt, HiCloudDownload } from 'react-icons/hi';
-import { DesktopCard } from './DesktopCard';
-import { HiChevronDown } from 'react-icons/hi';
-import { PrimaryButton } from '../../common/Buttons/PrimaryButton';
-import { AiFillGithub } from 'react-icons/ai';
+import {
+  HiTerminal,
+  HiFilm,
+  HiLightningBolt,
+  HiCloudDownload,
+  HiUserGroup,
+  HiViewBoards,
+  HiDocumentSearch,
+  HiPhoneOutgoing,
+  HiDatabase,
+  HiBell,
+  HiCode,
+  HiDesktopComputer,
+  HiPresentationChartLine,
+} from 'react-icons/hi'
+import { DesktopCard } from './DesktopCard'
+import { HiChevronDown } from 'react-icons/hi'
+import { PrimaryButton } from '../../common/Buttons/PrimaryButton'
+import { AiFillGithub } from 'react-icons/ai'
+import { ExclamationCircleFilled } from '@ant-design/icons'
 
 export type Feature = {
   title: string
@@ -19,12 +37,13 @@ export type Feature = {
   thumbnail: JSX.Element
   desktopImage: StaticImageData
   mobileImage: StaticImageData
-  feature1: string
-  featureImage1: JSX.Element
-  feature2: string
-  featureImage2: JSX.Element
-  feature3: string
-  featureImage3: JSX.Element
+  code?: string[]
+  feature1?: string
+  featureImage1?: JSX.Element
+  feature2?: string
+  featureImage2?: JSX.Element
+  feature3?: string
+  featureImage3?: JSX.Element
   link: string
 }
 
@@ -32,74 +51,77 @@ export type Feature = {
 //thumbnail needs classname "h-[40px] w-[40px]"
 const features: Feature[] = [
   {
-    title: "Error Monitoring",
-    description: "Error Monitoring for frontend & backend.",
-    thumbnail: <HiLightningBolt className="h-[35px] w-[35px]" />,
-    desktopImage: landingCarousel1,
+    title: 'Error Monitoring',
+    description:
+      'Understand the errors and exceptions happening in your web application.',
+    thumbnail: <HiTerminal className="h-[35px] w-[35px]" />,
+    desktopImage: errorMonitoring,
     mobileImage: tempCarouselImage,
-    feature1: "Console and Network Recording",
-    featureImage1: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature2: "Live Session Recording",
-    featureImage2: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature3: "Powerful Privacy Controls",
-    featureImage3: <HiTerminal className="h-[20px] w-[20px]" />,
-    link: "/docs/general/product-features/session-replay/overview",
+    feature1: 'Custom Error Grouping',
+    featureImage1: <HiUserGroup className="h-[20px] w-[20px]" />,
+    feature2: 'Customizable Alerting Rules',
+    featureImage2: <HiViewBoards className="h-[20px] w-[20px]" />,
+    feature3: 'Powered by Open Telemetry',
+    featureImage3: <ExclamationCircleFilled className="h-[20px] w-[20px]" />,
+    link: '/docs/general/product-features/error-monitoring/overview',
   },
   {
-    title: "Session Replay",
-    description: "Error Monitoring for frontend & backend.",
+    title: 'Session Replay',
+    description:
+      'Get to the real reasons that bugs are happening your web application.',
     thumbnail: <HiFilm className="h-[35px] w-[35px]" />,
-    desktopImage: landingCarousel1,
+    desktopImage: sessionReplay,
     mobileImage: tempCarouselImage,
-    feature1: "Console and Network Recording",
+    feature1: 'Console and Network Recording',
     featureImage1: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature2: "Live Session Recording",
-    featureImage2: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature3: "Powerful Privacy Controls",
-    featureImage3: <HiTerminal className="h-[20px] w-[20px]" />,
-    link: "/docs/general/product-features/session-replay/overview",
+    feature2: 'Comprehensive Session Search',
+    featureImage2: <HiDocumentSearch className="h-[20px] w-[20px]" />,
+    feature3: 'Powerful Privacy Controls',
+    featureImage3: <HiPhoneOutgoing className="h-[20px] w-[20px]" />,
+    link: '/docs/general/product-features/session-replay/overview',
   },
   {
-    title: "Logging",
-    description: "Error Monitoring for frontend & backend.",
+    title: 'Fullstack Logging',
+    description: 'Drill down on all the logs emitted throughout your stack.',
     thumbnail: <HiLightningBolt className="h-[35px] w-[35px]" />,
-    desktopImage: landingCarousel1,
+    desktopImage: fullstackLogging,
     mobileImage: tempCarouselImage,
-    feature1: "Console and Network Recording",
-    featureImage1: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature2: "Live Session Recording",
-    featureImage2: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature3: "Powerful Privacy Controls",
-    featureImage3: <HiTerminal className="h-[20px] w-[20px]" />,
-    link: "/docs/general/product-features/session-replay/overview",
+    feature1: 'Customizable Log Alerts',
+    featureImage1: <HiBell className="h-[20px] w-[20px]" />,
+    feature2: 'Widespread SDK Support',
+    featureImage2: <HiCode className="h-[20px] w-[20px]" />,
+    feature3: 'Powered by Clickhouse',
+    featureImage3: <HiDatabase className="h-[20px] w-[20px]" />,
+    link: '/docs/general/product-features/session-replay/overview',
   },
   {
-    title: "Selfhost",
-    description: "Error Monitoring for frontend & backend.",
+    title: 'Self-hosting Support',
+    description:
+      'Interested in self-hosting highlight? Spin up highlight.io in docker with just a few commands.',
     thumbnail: <HiCloudDownload className="h-[35px] w-[35px]" />,
-    desktopImage: landingCarousel1,
+    desktopImage: selfHosting,
     mobileImage: tempCarouselImage,
-    feature1: "Console and Network Recording",
-    featureImage1: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature2: "Live Session Recording",
-    featureImage2: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature3: "Powerful Privacy Controls",
-    featureImage3: <HiTerminal className="h-[20px] w-[20px]" />,
-    link: "/docs/general/product-features/session-replay/overview",
+    code: [
+      `git clone --recurse-submodules https://github.com/highlight/highlight;`,
+      `cd docker;`,
+      `docker compose up -d --build;`,
+    ],
+    link: '/docs/general/company/open-source/self-host-hobby',
   },
   {
-    title: "Open Source",
-    description: "Error Monitoring for frontend & backend.",
+    title: 'Open Source',
+    description:
+      'highlight.io is an open source tool for debugging your web application.',
     thumbnail: <AiFillGithub className="h-[35px] w-[35px]" />,
-    desktopImage: landingCarousel1,
+    desktopImage: openSource,
     mobileImage: tempCarouselImage,
-    feature1: "Console and Network Recording",
-    featureImage1: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature2: "Live Session Recording",
-    featureImage2: <HiTerminal className="h-[20px] w-[20px]" />,
-    feature3: "Powerful Privacy Controls",
-    featureImage3: <HiTerminal className="h-[20px] w-[20px]" />,
-    link: "/docs/general/product-features/session-replay/overview",
+    feature1: 'Join the Community',
+    featureImage1: <HiPresentationChartLine className="h-[20px] w-[20px]" />,
+    feature2: 'Find us on GitHub',
+    featureImage2: <HiDesktopComputer className="h-[20px] w-[20px]" />,
+    feature3: 'Self host highlight.io',
+    featureImage3: <ExclamationCircleFilled className="h-[20px] w-[20px]" />,
+    link: 'https://github.com/highlight/highlight',
   },
 ]
 
@@ -114,39 +136,76 @@ export const FeatureCarousel = () => {
         setSelected(emblaApi.selectedScrollSnap())
       })
     }
-    (document.getElementById("dropdown") as HTMLSelectElement).value = selected.toString()
+    ;(document.getElementById('dropdown') as HTMLSelectElement).value =
+      selected.toString()
   }, [emblaApi, selected])
 
   const handleDropdown = () => {
-    let dropdownValue = parseInt((document.getElementById("dropdown") as HTMLSelectElement).value)
+    let dropdownValue = parseInt(
+      (document.getElementById('dropdown') as HTMLSelectElement).value,
+    )
     setSelected(dropdownValue)
   }
 
   return (
     <div className="flex flex-col overflow-x-hidden lg:rounded-lg max-w-[100vw] xl:max-w-[1000px] lg:rounded-tr-lg lg:rounded-tl-lg">
       <div className={`hidden md:grid grid-cols-5`}>
-        {features.map((feature, index) =>
-          <div key={index} onClick={() => setSelected(index)} className={classNames(styles.carouselButton, "group", index == 0 ? "lg:rounded-tl-lg" : "", index == features.length - 1 ? "lg:rounded-tr-lg" : "")}>
-            <div className={`${selected == index ? "bg-divider-on-dark" : "group-hover:bg-divider-on-dark group-hover:bg-opacity-75"} h-full flex justify-center gap-1 px-3 py-2 rounded-lg transition-all`}>
+        {features.map((feature, index) => (
+          <div
+            key={index}
+            onClick={() => setSelected(index)}
+            className={classNames(
+              styles.carouselButton,
+              'group',
+              index == 0 ? 'lg:rounded-tl-lg' : '',
+              index == features.length - 1 ? 'lg:rounded-tr-lg' : '',
+            )}
+          >
+            <div
+              className={`${
+                selected == index
+                  ? 'bg-divider-on-dark'
+                  : 'group-hover:bg-divider-on-dark group-hover:bg-opacity-75'
+              } h-full flex justify-center gap-1 px-3 py-2 rounded-lg transition-all`}
+            >
               <div className="flex flex-col gap-1">
                 <div className="flex justify-center text-color-copy-on-dark">
                   {feature.thumbnail}
                 </div>
-                <Typography type="copy3" className="text-center" emphasis={true}>{feature.title}</Typography>
+                <Typography
+                  type="copy3"
+                  className="text-center"
+                  emphasis={true}
+                >
+                  {feature.title}
+                </Typography>
               </div>
             </div>
           </div>
-        )}
+        ))}
       </div>
       <div className="relative flex md:hidden justify-center mx-5 mb-8 rounded-lg">
-        <PrimaryButton className={classNames(styles.whiteButton, "w-full border-copy-on-dark py-0 px-0 h-[54px]")}>
-          <Typography type="copy2" className="bg-color-primary-500 rounded-lg w-full" emphasis={true}>
-            <select onChange={() => handleDropdown()} id="dropdown" className="w-full px-4 bg-color-primary-500 h-full text-center rounded-lg appearance-none">
-              {features.map((feature, index) =>
+        <PrimaryButton
+          className={classNames(
+            styles.whiteButton,
+            'w-full border-copy-on-dark py-0 px-0 h-[54px]',
+          )}
+        >
+          <Typography
+            type="copy2"
+            className="bg-color-primary-500 rounded-lg w-full"
+            emphasis={true}
+          >
+            <select
+              onChange={() => handleDropdown()}
+              id="dropdown"
+              className="w-full px-4 bg-color-primary-500 h-full text-center rounded-lg appearance-none"
+            >
+              {features.map((feature, index) => (
                 <option key={index} value={index}>
                   {feature.title}
                 </option>
-              )}
+              ))}
             </select>
           </Typography>
         </PrimaryButton>
@@ -154,11 +213,11 @@ export const FeatureCarousel = () => {
       </div>
       <div className="w-screen lg:w-full" ref={emblaRef}>
         <div className="flex scrollbar-hide lg:gap-4">
-          {features.map((feature, index) =>
+          {features.map((feature, index) => (
             <DesktopCard key={index} feature={feature} index={index} />
-          )}
+          ))}
         </div>
       </div>
-    </div >
+    </div>
   )
-};
+}
