03d951f62fa7796999eb61d329a4aeb646f31de6
Crawron <azertyaaron@gmail.com>
2023-02-15 16:19:46 -0500

    center the plans section

--- pages/pricing/v2-draft.tsx
+++ pages/pricing/v2-draft.tsx
@@ -14,7 +14,7 @@ import classNames from "classnames";
 const PricingPage: NextPage = () => {
 	return <div >
 		<Navbar />
-		<div className="flex flex-col w-full max-w-screen-xl px-10 mx-auto mt-24">
+		<div className="flex flex-col w-full px-10 mx-auto mt-24">
 			<div className="flex flex-col items-center text-center gap-9"> {/* Title, tagline and CTA */}
 				<h1 className="max-w-3xl">Get the <span className="text-highlight-yellow">visibility</span> you need today.</h1>
 				<Typography type="copyHeader" onDark>Fair and transparent pricing that scales with any organization.</Typography>
@@ -23,39 +23,37 @@ const PricingPage: NextPage = () => {
 					<PrimaryButton href="#" className={homeStyles.hollowButton}>Chat with us</PrimaryButton>
 				</div>
 			</div>
-			<div className="flex flex-col items-center mt-16"> {/* Pricing */}
-				<div className="flex gap-11">
-					<div className="flex flex-col w-48 gap-11">
-						<PricingRadioFilter title="Billing Period" options={["Monthly", "Annual"]} />
-						<PricingRadioFilter title="Retention" options={["3 Months", "6 Months", "12 Months", "2 years"]} />
-					</div>
-					<div className="flex flex-col">
-						<div className="flex justify-between w-[1100px]">
-							<PriceItem title="Free" pricePerMonth="0" >
-								<span>500 monthly sessions</span>
-								<span>1000 monthly errors</span>
-								<span>Unlimited seats</span>
-							</PriceItem>
-							<PriceItem title="Basic" pricePerMonth="50" >
-								<span>500 monthly sessions</span>
-								<span>1000 monthly errors</span>
-								<span>Unlimited seats</span>
-							</PriceItem>
-							<PriceItem title="Essentials" pricePerMonth="150" >
-								<span>500 monthly sessions</span>
-								<span>Unlimited seats</span>
-							</PriceItem>
-							<PriceItem title="Startup" pricePerMonth="400" >
-								<span>500 monthly sessions</span>
-								<span>1000 monthly errors</span>
-								<span>Placeholder item</span>
-								<span>Unlimited seats</span>
-							</PriceItem>
-						</div>
-						<Typography type="copy1" onDark className="text-center my-9">If usage goes beyond the included monthly quota, your <a href="#">usage rate</a> kicks in.</Typography>
-					</div>
-					{/* <div className="flex-shrink w-48"/> */}
+			<div className="flex max-w-full mx-auto mt-16 gap-11"> {/* Pricing */}
+				<div className="flex flex-col flex-shrink-0 w-48 gap-11">
+					<PricingRadioFilter title="Billing Period" options={["Monthly", "Annual"]} />
+					<PricingRadioFilter title="Retention" options={["3 Months", "6 Months", "12 Months", "2 years"]} />
 				</div>
+				<div className="flex flex-col">
+					<div className="flex justify-between w-[1100px]">
+						<PriceItem title="Free" pricePerMonth="0" >
+							<span>500 monthly sessions</span>
+							<span>1000 monthly errors</span>
+							<span>Unlimited seats</span>
+						</PriceItem>
+						<PriceItem title="Basic" pricePerMonth="50" >
+							<span>500 monthly sessions</span>
+							<span>1000 monthly errors</span>
+							<span>Unlimited seats</span>
+						</PriceItem>
+						<PriceItem title="Essentials" pricePerMonth="150" >
+							<span>500 monthly sessions</span>
+							<span>Unlimited seats</span>
+						</PriceItem>
+						<PriceItem title="Startup" pricePerMonth="400" >
+							<span>500 monthly sessions</span>
+							<span>1000 monthly errors</span>
+							<span>Placeholder item</span>
+							<span>Unlimited seats</span>
+						</PriceItem>
+					</div>
+					<Typography type="copy1" onDark className="text-center my-9">If usage goes beyond the included monthly quota, your <a href="#">usage rate</a> kicks in.</Typography>
+				</div>
+				<div className="flex-shrink w-48" />
 			</div>
 			<div className="flex flex-col items-center mt-32 text-center gap-9"> {/* Pay as you go */}
 				<h2>Pay <span className="text-highlight-yellow">as you go.</span></h2>
