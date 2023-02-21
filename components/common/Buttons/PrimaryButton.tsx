6ccbc05fc69a117a563e1d911682ff776f287672
Jay Khatri <jaykhatrimail@gmail.com>
2023-02-20 18:29:01 -0800

    progress

--- components/common/Buttons/PrimaryButton.tsx
+++ components/common/Buttons/PrimaryButton.tsx
@@ -8,12 +8,31 @@ export const PrimaryButton = ({
   children,
   ...props
 }: React.PropsWithChildren<ButtonProps>) => {
+
+  const reportSignupConversion = (url: string) => {
+    var callback = function () {
+      console.log("running callback");
+      if (typeof (url) != 'undefined') {
+        window.location.href = url;
+      }
+    };
+    console.log("about to send conversion event");
+    window.gtag('event', 'conversion', {
+      'send_to': 'AW-10833687189/q81TCPDrq40DEJXl860o',
+      'event_callback': callback
+    });
+    console.log("sent conversion event");
+  }
+
   return (
     <a
       {...props}
       onClick={(e) => {
         if (props.onClick) {
           props.onClick(e);
+        }
+        if (props.href && props.href.includes('app.highlight.io/?sign_up=1')) {
+          reportSignupConversion(props.href);
         }
       }}
       className={classNames(
