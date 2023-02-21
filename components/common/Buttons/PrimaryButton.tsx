8da47d0e388c16c09d3abe029be78a7527c3a1c5
Jay Khatri <jaykhatrimail@gmail.com>
2023-02-20 18:45:08 -0800

    try

--- components/common/Buttons/PrimaryButton.tsx
+++ components/common/Buttons/PrimaryButton.tsx
@@ -4,35 +4,38 @@ import classNames from 'classnames';
 
 import styles from './Buttons.module.scss';
 
+function gtag_report_conversion(url) {
+}
+
 export const PrimaryButton = ({
   children,
   ...props
 }: React.PropsWithChildren<ButtonProps>) => {
 
-  const reportSignupConversion = (url: string) => {
-    var callback = function () {
-      console.log("running callback");
-      if (typeof (url) != 'undefined') {
-        window.location.href = url;
-      }
-    };
-    console.log("about to send conversion event");
-    window.gtag('event', 'conversion', {
-      'send_to': 'AW-10833687189/q81TCPDrq40DEJXl860o',
-      'event_callback': callback
-    });
-    console.log("sent conversion event");
-  }
-
   return (
     <a
       {...props}
       onClick={(e) => {
-        if (props.onClick) {
-          props.onClick(e);
-        }
-        if (props.href && props.href.includes('app.highlight.io/?sign_up=1')) {
-          reportSignupConversion(props.href);
+        if (props.href?.includes("app.highlight.io/sign_up=1")) {
+          e.preventDefault();
+          if (props.onClick) {
+            props.onClick(e);
+          }
+          var callback = function () {
+            if (typeof (props.href) != 'undefined') {
+              window.location = props.href;
+            }
+          };
+          gtag('event', 'conversion', {
+            'send_to': 'AW-10833687189/q81TCPDrq40DEJXl860o',
+            'event_callback': callback
+          });
+          return false;
+
+        } else {
+          if (props.onClick) {
+            props.onClick(e);
+          }
         }
       }}
       className={classNames(
