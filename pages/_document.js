b3767dab4d460d66c3a7c648f6275aefff4fa876
Jay Khatri <jaykhatrimail@gmail.com>
2023-02-20 18:05:26 -0800

    anotha one

--- pages/_document.js
+++ pages/_document.js
@@ -13,17 +13,30 @@ class HighlightDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital@0;1&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           ></link>
-          <NextScript async src="https://www.googletagmanager.com/gtag/js?id=AW-10833687189"/>
-          <script>
+          {/* From the "Highlight Main Site" gtag */}
+          <NextScript async src="https://www.googletagmanager.com/gtag/js?id=G-P2T64QS94S"></NextScript>
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
             window.dataLayer = window.dataLayer || [];
             function gtag(){dataLayer.push(arguments);}
             gtag('js', new Date());
 
-            gtag('config', 'AW-10833687189');
-          </script>
+            gtag('config', 'G-P2T64QS94S');
+          `,
+            }}
+          />
+          <script
+            dangerouslySetInnerHTML={{
+              __html: `
+          gtag('config', 'AW-10833687189');
+          `,
+            }}
+          ></script>
         </Head>
         <body style={{ overflowX: 'hidden' }}>
           <Main />
+          <NextScript />
         </body>
       </Html>
     )
