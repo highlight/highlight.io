6215a64a1995b95cf52624c4883e9187405b59a6
Jay Khatri <jaykhatrimail@gmail.com>
2023-02-20 17:48:19 -0800

    add google tag

--- pages/_document.js
+++ pages/_document.js
@@ -13,28 +13,17 @@ class HighlightDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=IBM+Plex+Mono:ital@0;1&family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap"
             rel="stylesheet"
           ></link>
+          <NextScript async src="https://www.googletagmanager.com/gtag/js?id=AW-10833687189"/>
           <script>
-            dangerouslySetInnerHTML=
-            {{
-              __html: `
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-PGVFG82');
-              `,
-            }}
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'AW-10833687189');
           </script>
         </Head>
         <body style={{ overflowX: 'hidden' }}>
           <Main />
-          <NextScript />
-          <noscript
-            dangerouslySetInnerHTML={{
-              __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=GTM-PGVFG82"
-              height="0" width="0" style="display:none;visibility:hidden"></iframe>`,
-            }}
-          />
         </body>
       </Html>
     )
