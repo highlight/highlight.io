6d2ea04395417f65132d252a6e8b9655c52241d9
Jay Khatri <jaykhatrimail@gmail.com>
2023-03-06 22:46:51 -0800

    remove conversion event

--- components/common/Buttons/PrimaryButton.tsx
+++ components/common/Buttons/PrimaryButton.tsx
@@ -6,31 +6,7 @@ import styles from './Buttons.module.scss'
 
 export const PrimaryButton = ({ children, ...props }: React.PropsWithChildren<ButtonProps>) => {
   return (
-    <a
-      {...props}
-      onClick={(e) => {
-        if (window.dataLayer && props.href?.includes('app.highlight.io/?sign_up=1')) {
-          e.preventDefault()
-          if (props.onClick) {
-            props.onClick(e)
-          }
-          window.gtag('event', 'conversion', {
-            send_to: 'AW-10833687189/_C5MCLfmoY0YEJXl860o',
-            event_callback: function () {
-              if (props.href) {
-                window.location = props.href as any as Location
-              }
-            },
-          })
-          return false
-        } else {
-          if (props.onClick) {
-            props.onClick(e)
-          }
-        }
-      }}
-      className={classNames(props.className, styles.genericButton, styles.primaryButton)}
-    >
+    <a {...props} className={classNames(props.className, styles.genericButton, styles.primaryButton)}>
       {children}
     </a>
   )
