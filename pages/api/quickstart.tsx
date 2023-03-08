6f7a0e509babaaed3d35bfe9f4f7cc02a1b5c4c9
Chris Schmitz <ccschmitz@gmail.com>
2023-03-08 13:31:15 -0600

    Update CORS options

--- pages/api/quickstart.tsx
+++ pages/api/quickstart.tsx
@@ -4,10 +4,8 @@ import { quickStartContent } from '../../components/QuickstartContent/Quickstart
 
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
   await NextCors(req, res, {
-    // Options
-    methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
+    methods: ['GET'],
     origin: '*',
-    optionsSuccessStatus: 200,
   })
 
   res.status(200).json(quickStartContent)
