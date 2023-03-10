8031e2f75893d4674c02cadb4413f02ff2b8fcab
Vadim Korolik <vkorolik@gmail.com>
2023-03-09 23:22:29 -0800

    update attribution previous referral processing

--- utils/attribution.ts
+++ utils/attribution.ts
@@ -1,18 +1,34 @@
 import Cookies from 'js-cookie'
 
+interface Referrer {
+  utm_source: string | null
+  utm_medium: string | null
+  utm_campaign: string | null
+  utm_content: string | null
+  utm_term: string | null
+  device: string | null
+  gclid: string | null
+  referrer: string | null
+}
+
 // Same as what we have in frontend. Need to keep these in sync.
 export const setAttributionData = () => {
-  const urlParams = new URLSearchParams(window.location.search)
-  const referrer = {
-    utm_source: urlParams.get('utm_source'),
-    utm_medium: urlParams.get('utm_medium'),
-    utm_campaign: urlParams.get('utm_campaign'),
-    utm_content: urlParams.get('utm_content'),
-    utm_term: urlParams.get('utm_term'),
-    device: urlParams.get('device'),
-    gclid: urlParams.get('gclid'),
-    referrer: urlParams.get('ref') || document.referrer,
-    previousReferrer: Cookies.get('referrer'),
+  let referrer: Referrer
+  const prevRef = Cookies.get('referrer')
+  if (prevRef) {
+    referrer = JSON.parse(prevRef) as Referrer
+  } else {
+    const urlParams = new URLSearchParams(window.location.search)
+    referrer = {
+      utm_source: urlParams.get('utm_source'),
+      utm_medium: urlParams.get('utm_medium'),
+      utm_campaign: urlParams.get('utm_campaign'),
+      utm_content: urlParams.get('utm_content'),
+      utm_term: urlParams.get('utm_term'),
+      device: urlParams.get('device'),
+      gclid: urlParams.get('gclid'),
+      referrer: urlParams.get('ref') || document.referrer,
+    }
   }
   Cookies.set('referrer', JSON.stringify(referrer), { domain: 'highlight.io' })
 }
