e3aed80848c7e4b444bb0ff07eefcec396ede2f3
Vadim Korolik <vkorolik@gmail.com>
2023-03-10 16:27:00 -0800

    update

--- components/QuickstartContent/logging/go/other.tsx
+++ components/QuickstartContent/logging/go/other.tsx
@@ -28,11 +28,16 @@ func main() {
 	
   // setup highlight logrus hook
   hlog.Init()
+  // if you don't want to get stdout / stderr output, add the following uncommented
+  // hlog.DisableOutput()
   
   // if in a request, provide context to associate logs with frontend sessions
   ctx := context.TODO()
   // send logs
   logrus.WithContext(ctx).WithField("hello", "world").Info("welcome to highlight.io")
+  // send logs with a string message severity
+  lvl, _ := logrus.ParseLevel("warn")
+  logrus.WithContext(ctx).Log(lvl, "whoa there")
 }`,
         language: 'go',
       },
