259ac85c66d5c0b7ccac107eb944eebb3a9dd5b3
Jay Khatri <jaykhatrimail@gmail.com>
2023-03-11 19:55:28 -0800

    change

--- components/GithubPopup/GithubPopup.tsx
+++ components/GithubPopup/GithubPopup.tsx
@@ -1,19 +1,44 @@
 import { useEffect, useState } from 'react'
 import GitHubButton from 'react-github-btn'
-import CrossIcon from '../../public/images/CrossIcon'
+
 import { motion } from 'framer-motion'
-import styles from './GithubPopup.module.scss'
+import CrossIcon from '../../public/images/CrossIcon'
 import { useMediaQuery } from '../MediaQuery/MediaQuery'
+import styles from './GithubPopup.module.scss'
 
 import { AnimateIn } from '../Animate'
 
 export const GithubPopup = () => {
   const is400 = useMediaQuery(400)
-  return <>{is400 ? <MobileGithubPopup /> : <DesktopGithubPopup />}</>
+  const [lastPageLoadTime, setLastPageLoadTime] = useState<number | undefined>(undefined)
+
+  useEffect(() => {
+    const lplt = JSON.parse(localStorage.getItem('lastPageLoadTime') ?? '')
+    if (lplt) {
+      setLastPageLoadTime(lplt)
+    }
+  }, [setLastPageLoadTime])
+
+  useEffect(() => {
+    let timer1 = setTimeout(() => {
+      localStorage.setItem('lastPageLoadTime', JSON.stringify(new Date().getTime()))
+    }, 1000)
+    return () => {
+      clearTimeout(timer1)
+    }
+  })
+
+  const popup = <>{is400 ? <MobileGithubPopup /> : <DesktopGithubPopup />}</>
+
+  const delta = lastPageLoadTime && new Date().getTime() - lastPageLoadTime
+
+  // don't render if a render happened 500 seconds ago.
+  return delta && delta > 500 * 1000 ? popup : <></>
 }
 
 const MobileGithubPopup = () => {
   const [hide, setHide] = useState(false)
+
   return hide ? (
     <></>
   ) : (
