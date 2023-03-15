2637c9382b67045e17d4abda15e4e0926ed584c4
Eric Thomas <eric@highlight.io>
2023-03-15 09:53:50 -0600

    specify minium requiremesnts for disk space

--- components/QuickstartContent/self-host/self-host.tsx
+++ components/QuickstartContent/self-host/self-host.tsx
@@ -7,7 +7,7 @@ export const SelfHostContent: QuickStartContent = {
     {
       title: 'Prerequisites',
       content:
-        'Before we get started, you should have the latest version of [docker](https://docs.docker.com/engine/install/) (19.03.0+) and [git](https://git-scm.com/downloads) (2.13+) installed. For a local deploy, we suggest [configuring docker](https://docs.docker.com/desktop/settings/mac/#resources) to use at least 16GB of memory and 4 CPUs.',
+        'Before we get started, you should have the latest version of [docker](https://docs.docker.com/engine/install/) (19.03.0+) and [git](https://git-scm.com/downloads) (2.13+) installed. For a local deploy, we suggest [configuring docker](https://docs.docker.com/desktop/settings/mac/#resources) to use at least 16GB of memory, 4 CPUs, and 50 GB of disk space.',
     },
     {
       title: 'Clone the repository.',
