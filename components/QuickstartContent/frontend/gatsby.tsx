367011f4f3495e523bd9f2a4506edcbd3f7e2725
Jay Khatri <jaykhatrimail@gmail.com>
2023-03-20 08:47:24 -0700

    Replace `appVersion` with `version` (#445)
    I think `appVersion` isn't used in our js snippet. @Vadman97 take a
    quick look and confirm if you don't mind?

--- components/QuickstartContent/frontend/gatsby.tsx
+++ components/QuickstartContent/frontend/gatsby.tsx
@@ -10,7 +10,7 @@ import {
 export const initializePluginSnippet: QuickStartStep = {
   title: 'Initialize the plugin in your gatsby configuration.',
   content: `Grab your project ID from [app.highlight.io/setup](https://app.highlight.io/setup) and insert it in place of \`<YOUR_PROJECT_ID>\`.  
-                    To get started, we recommend setting \`environment\`, \`appVersion\`, and \`networkRecording\`. Refer to our docs on [SDK configuration](${sessionReplayFeaturesLink}) to read more about these options. `,
+                    To get started, we recommend setting \`environment\`, \`version\`, and \`networkRecording\`. Refer to our docs on [SDK configuration](${sessionReplayFeaturesLink}) to read more about these options. `,
   code: {
     text: `module.exports = {
 	plugins: [
