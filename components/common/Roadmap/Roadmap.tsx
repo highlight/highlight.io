6af4947496ffe8dc84f4a2ff6ba3fc712665fce6
Abhishek More <abhimore74@gmail.com>
2023-03-22 16:35:16 -0500

    button widths

--- components/common/Roadmap/Roadmap.tsx
+++ components/common/Roadmap/Roadmap.tsx
@@ -13,16 +13,19 @@ export const Roadmap = (content: { content: RoadmapProps }) => {
       <div className="flex flex-col sm:flex-row gap-4 mt-4">
         <PrimaryButton
           href="https://github.com/highlight/highlight/issues/new?assignees=&labels=external+bug+%2F+request&template=feature_request.md&title="
-          className={classNames(styles.solidButton, 'inline-block py-[6px] items-center px-3 rounded-md')}
+          className={classNames(styles.solidButton, 'inline-block py-[6px] items-center px-3 rounded-md min-w-[185px]')}
         >
           <BsGithub className="inline-block mr-2 mb-[3px] h-[18px]" />
           <Typography type="copy3" emphasis>
-            Submit Feature Request
+            Feature Request
           </Typography>
         </PrimaryButton>
         <PrimaryButton
           href="https://github.com/orgs/highlight/projects/11/views/1"
-          className={classNames(styles.hollowButton, 'inline-block py-[6px]  items-center rounded-md')}
+          className={classNames(
+            styles.hollowButton,
+            'inline-block py-[6px] px-3 items-center rounded-md min-w-[185px]',
+          )}
         >
           <BsGithub className="inline-block mr-2 mb-[3px] h-[18px] " />
           <Typography type="copy3" emphasis>
